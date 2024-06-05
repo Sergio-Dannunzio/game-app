2254cf6c511e12926036d9fbadbc33fc5cec3135
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2024-06-05 08:32:34 -0300

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,5 @@ import { Routes } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 
 export const routes: Routes = [
-    {
-        path:"home",
-        component: HeroComponent,
-    },
-    {
-        path:"",
-        redirectTo: "home",
-        pathMatch:"full",
-    },
-    {
-        path:"**",
-        redirectTo: "home",
-        pathMatch:"full",
-    },
+
 ];
