51b3d0a648ec266424dc7e48214638c3b2cefd96
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2024-06-05 08:37:09 -0300

    fix ghpages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7R6ZBODS.mjs";import"./chunk-7POUKTPP.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-SLBLY5WM.mjs";import"./chunk-7CTDJDQF.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
